9a456bbdadcf72603b9199d7ba4b0663cb52c6bf
MishaStryzhka <MishaStryzhka@users.noreply.github.com>
2024-09-11 20:24:27 +0000

    deploy: ab1f2829b41ac46dc749043763bb7d8be5f2661d

--- bundle.js
+++ bundle.js
@@ -548,7 +548,7 @@ eval("module.exports = __webpack_require__.p + \"d33aecce785b91c7f50c.png\";\n\n
 /******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
 /******/ 		var document = __webpack_require__.g.document;
 /******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
+/******/ 			if (document.currentScript && document.currentScript.tagName.toUpperCase() === 'SCRIPT')
 /******/ 				scriptUrl = document.currentScript.src;
 /******/ 			if (!scriptUrl) {
 /******/ 				var scripts = document.getElementsByTagName("script");
