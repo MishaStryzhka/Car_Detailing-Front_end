62be2f7e90989e17c96a1ab5913e0def136e500a
MishaStryzhka <MishaStryzhka@users.noreply.github.com>
2024-09-24 21:03:02 +0000

    deploy: dbb7c2891a008e0556f43122ae8adeb2d51f1dac

--- bundle.js
+++ bundle.js
@@ -16,7 +16,7 @@
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _js_setlanguage__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/setlanguage */ \"./src/js/setlanguage.js\");\n/* harmony import */ var _js_setlanguage__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_js_setlanguage__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _js_burger_menu__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./js/burger-menu */ \"./src/js/burger-menu.js\");\n/* harmony import */ var _js_burger_menu__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_js_burger_menu__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _styles_main_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./styles/main.scss */ \"./src/styles/main.scss\");\n/* harmony import */ var _js_set_slide_per_view__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./js/set-slide-per-view */ \"./src/js/set-slide-per-view.js\");\n/* harmony import */ var _js_set_slide_per_view__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_js_set_slide_per_view__WEBPACK_IMPORTED_MODULE_3__);\n/* harmony import */ var _js_setParamsReviewsSwiper__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./js/setParamsReviewsSwiper */ \"./src/js/setParamsReviewsSwiper.js\");\n/* harmony import */ var _js_setParamsReviewsSwiper__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_js_setParamsReviewsSwiper__WEBPACK_IMPORTED_MODULE_4__);\n\n\n\n\n\n\n//# sourceURL=webpack://car_detailing-front_end/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _js_setlanguage__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/setlanguage */ \"./src/js/setlanguage.js\");\n/* harmony import */ var _js_setlanguage__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_js_setlanguage__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _js_burger_menu__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./js/burger-menu */ \"./src/js/burger-menu.js\");\n/* harmony import */ var _js_burger_menu__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_js_burger_menu__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _styles_main_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./styles/main.scss */ \"./src/styles/main.scss\");\n/* harmony import */ var _js_set_slide_per_view__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./js/set-slide-per-view */ \"./src/js/set-slide-per-view.js\");\n/* harmony import */ var _js_set_slide_per_view__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_js_set_slide_per_view__WEBPACK_IMPORTED_MODULE_3__);\n/* harmony import */ var _js_setParamsReviewsSwiper__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./js/setParamsReviewsSwiper */ \"./src/js/setParamsReviewsSwiper.js\");\n/* harmony import */ var _js_setParamsReviewsSwiper__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_js_setParamsReviewsSwiper__WEBPACK_IMPORTED_MODULE_4__);\n/* harmony import */ var _js_setOpenLanguageList__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./js/setOpenLanguageList */ \"./src/js/setOpenLanguageList.js\");\n/* harmony import */ var _js_setOpenLanguageList__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_js_setOpenLanguageList__WEBPACK_IMPORTED_MODULE_5__);\n\n\n\n\n\n\n\n//# sourceURL=webpack://car_detailing-front_end/./src/index.js?");
 
 /***/ }),
 
@@ -40,6 +40,16 @@ eval("// swiper element\nvar swiperEl = document.querySelector('[data-swiper-sec
 
 /***/ }),
 
+/***/ "./src/js/setOpenLanguageList.js":
+/*!***************************************!*\
+  !*** ./src/js/setOpenLanguageList.js ***!
+  \***************************************/
+/***/ (() => {
+
+eval("(function () {\n  function setupLanguageToggle(version) {\n    var refs = {\n      openListBtn: document.querySelector(\"[data-language-toggle=\\\"\".concat(version, \"\\\"]\")),\n      list: document.querySelector(\"[data-language-list=\\\"\".concat(version, \"\\\"]\")),\n      arrow: document.querySelector(\"[data-arrow=\\\"\".concat(version, \"\\\"]\"))\n    };\n    if (refs.openListBtn && refs.list && refs.arrow) {\n      refs.openListBtn.addEventListener('click', function () {\n        toggleMenu(refs);\n      });\n    } else {\n      console.error(\"\\u0415\\u043B\\u0435\\u043C\\u0435\\u043D\\u0442\\u0438 \\u043D\\u0435 \\u0437\\u043D\\u0430\\u0439\\u0434\\u0435\\u043D\\u0456 \\u0434\\u043B\\u044F \\u0432\\u0435\\u0440\\u0441\\u0456\\u0457: \".concat(version), refs);\n    }\n  }\n  function toggleMenu(refs) {\n    refs.list.classList.toggle('hidden');\n    refs.arrow.classList.toggle('rotate');\n  }\n\n  // Налаштування для десктопної та мобільної версій\n  setupLanguageToggle('desktop');\n  setupLanguageToggle('burger');\n})();\n\n//# sourceURL=webpack://car_detailing-front_end/./src/js/setOpenLanguageList.js?");
+
+/***/ }),
+
 /***/ "./src/js/setParamsReviewsSwiper.js":
 /*!******************************************!*\
   !*** ./src/js/setParamsReviewsSwiper.js ***!
@@ -56,7 +66,7 @@ eval("var swiperEl = document.querySelector('.reviews-swiper');\nif (swiperEl) {
   \*******************************/
 /***/ (() => {
 
-eval("document.addEventListener('DOMContentLoaded', function () {\n  var currentPath = window.location.pathname;\n  var lastSegment = currentPath.split('/').pop();\n  var languageLinks = document.querySelectorAll('.wrap-languages a');\n  languageLinks.forEach(function (link) {\n    var baseHref = link.getAttribute('href');\n    if (currentPath === baseHref + lastSegment) {\n      link.classList.add('active');\n      return;\n    }\n    link.setAttribute('href', baseHref + lastSegment);\n  });\n});\n\n//# sourceURL=webpack://car_detailing-front_end/./src/js/setlanguage.js?");
+eval("document.addEventListener('DOMContentLoaded', function () {\n  var currentPath = window.location.pathname;\n  var lastSegment = currentPath.split('/').pop();\n  var languageLinks = document.querySelectorAll('.wrap-languages a');\n  var languageMenuLinks = document.querySelectorAll('.language-list a');\n  languageLinks.forEach(function (link) {\n    var baseHref = link.getAttribute('href');\n    if (currentPath === baseHref + lastSegment) {\n      link.classList.add('active');\n      return;\n    }\n    link.setAttribute('href', baseHref + lastSegment);\n  });\n  languageMenuLinks.forEach(function (link) {\n    var baseHref = link.getAttribute('href');\n    if (currentPath === baseHref + lastSegment) {\n      link.classList.add('active');\n      return;\n    }\n    link.setAttribute('href', baseHref + lastSegment);\n  });\n});\n\n//# sourceURL=webpack://car_detailing-front_end/./src/js/setlanguage.js?");
 
 /***/ }),
 
